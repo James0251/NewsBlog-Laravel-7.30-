81e94ec10712276533471936623df440bc8e95c6
James0251 <den4643@yandex.ru>
2022-10-18 17:56:20 +0300

    разделил шаблон админки по отдельным файлам в admin/layouts

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
@@ -12,7 +15,7 @@ class DashboardController extends Controller
      * Handle the incoming request.
      *
      * @param Request $request
-     * @return Response
+     * @return Application|Factory|View
      */
     public function __invoke(Request $request)
     {
--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class IndexController extends Controller
 {
@@ -12,7 +15,7 @@ class IndexController extends Controller
      * Handle the incoming request.
      *
      * @param Request $request
-     * @return Response
+     * @return Application|Factory|View
      */
     public function __invoke(Request $request)
     {
