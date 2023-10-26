e0c61a137a6b5141fdc2111d298fb5ec24772238
James0251 <den4643025@gmail.com>
2023-10-26 11:33:24 +0300

    Пришлось снова добавить всё для шаблона, чтобы в результате оптимизировать css/style.css

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 /**
-* Template Name: Moderna - v4.8.0
+* Template Name: Moderna
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
 * Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
@@ -226,5 +227,10 @@
       mirror: false
     });
   });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
 
 })()
