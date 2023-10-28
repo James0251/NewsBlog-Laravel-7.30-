609c00f863fb164edbdf3fe02dc45f1541096c4c
James0251 <den4643025@gmail.com>
2023-10-28 11:34:32 +0300

    Отредактировал главный шаблон layouts/main/index.blade.php, все необходимые зависимости находятся в папке main и подключаются в шаблон

---
+++ resources/views/layouts/main/header.blade.php
@@ -0,0 +1,59 @@
+{{-- HEADER ГЛАВНОГО ШАБЛОНА ДЛЯ ВСЕХ СТРАНИЦ --}}
+
+
+@if(\Illuminate\Support\Facades\Request::url() !== route('login'))
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+        <div class="container d-flex justify-content-between align-items-center">
+
+            <div class="logo">
+                <h1 class="text-light"><a href="index.html"><span>Moderna</span></a></h1>
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="active " href="{{ route('main') }}">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="team.html">Team</a></li>
+                    <li><a href="{{ route('blog.index') }}">Blog</a></li>
+                    <li><a href="contact.html">Contact Us</a></li>
+                    @auth
+                    <li class="dropdown"><a href="#"><span>{{ Auth::user()->name }}</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Личный кабинет</a></li>
+                            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                                <ul>
+                                    <li><a href="#">Deep Drop Down 1</a></li>
+                                    <li><a href="#">Deep Drop Down 2</a></li>
+                                    <li><a href="#">Deep Drop Down 3</a></li>
+                                    <li><a href="#">Deep Drop Down 4</a></li>
+                                    <li><a href="#">Deep Drop Down 5</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Drop Down 2</a></li>
+                            <li><a href="#">Drop Down 3</a></li>
+                            <li><a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                    @endauth
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header>
+    <!-- ======= End Header ======= -->
+@endif
+
---
+++ resources/views/layouts/main/style.blade.php
@@ -0,0 +1,33 @@
+{{-- Стили Главного Шаблона --}}
+
+
+<!-- Favicons -->
+<link href="{{ asset('img/favicon.png') }}" rel="icon">
+<link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+
+<!-- Vendor CSS Files -->
+<link href="{{ asset('vendor/animate.css/animate.min.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('css/unreg_user.css') }}" rel="stylesheet">
+
+
+<!-- =======================================================
+* Template Name: Moderna - v4.8.0
+* Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+======================================================== -->
