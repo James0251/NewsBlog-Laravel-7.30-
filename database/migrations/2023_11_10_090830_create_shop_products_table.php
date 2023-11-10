279696ff66b870907d9f9081392806f9c4a46238
James0251 <den4643025@gmail.com>
2023-11-10 12:43:17 +0300

    Создал модели Shop/ShopCategory и Shop/ShopProduct и миграции к ним

---
+++ database/migrations/2023_11_10_090830_create_shop_products_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('category_id')->unsigned()->nullable();
+            $table->string('article');
+            $table->string('name');
+            $table->string('slug')->unique();
+            // Короткое описание
+            $table->string('short_description')->nullable();
+            // Описание
+            $table->text('description');
+            $table->decimal('regular_price');
+            $table->decimal('sale_price')->nullable();
+            $table->string('SKU');
+            $table->enum('stock_status', ['instock', 'outofstock']);
+            $table->boolean('featured')->default(false);
+            $table->unsignedInteger('quantity')->default(10);
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('shop_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_products');
+    }
+}
